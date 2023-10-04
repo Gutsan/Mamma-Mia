a9338fd7a81f74031276f56660bb77287cf1859f
Bastian Gutiérrez <bas.gutierrez@gmail.com>
2023-10-04 13:58:36 -0300

    creación de componente header

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { CardContexProvider } from "./Context/CardContex";
 import { Home } from "./view/Home";
 import { PizzaDetails } from "./view/PizzaDetails";
 import { PizzaContexProvider } from "./Context/PizzasContext";
+import { Header } from "./Components/Header";
 function App() {
   return (
     <BrowserRouter>
       <PizzaContexProvider>
         <CardContexProvider>
+          <Header/>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/:IdPizza" element={<PizzaDetails />} />
