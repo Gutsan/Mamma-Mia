6e867e9a9648be6c5008f967b5c63f67fadc64d0
Bastian Gutiérrez <bas.gutierrez@gmail.com>
2023-10-05 14:17:38 -0300

    se crea hero section

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { Home } from "./view/Home";
 import { PizzaDetails } from "./view/PizzaDetails";
 import { PizzaContexProvider } from "./Context/PizzasContext";
 import { Header } from "./Components/Header";
+import { HeroSection } from "./Components/Hero-Section";
 function App() {
   return (
     <BrowserRouter>
       <PizzaContexProvider>
         <CardContexProvider>
           <Header/>
+          <HeroSection/>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/:IdPizza" element={<PizzaDetails />} />
---
+++ src/Components/Hero-Section.jsx
@@ -0,0 +1,10 @@
+export const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <img
+        src="https://th.bing.com/th/id/OIP.Te7PacFMg9J8eKALGndIUAHaEK?pid=ImgDet&rs=1"
+        alt="imagen principal"
+      />
+    </div>
+  );
+};
